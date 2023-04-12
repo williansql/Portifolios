742b579fa1f417b8ef42c7f8bf92f0e668d88bf9
Willians Lisardo <williansql@outlook.com>
2023-04-12 17:57:19 -0300

    Finalização do template e funções do array do formulario com radio options

--- formularioArray/src/app/cadastro-editais/cadastro-editais.component.ts
+++ formularioArray/src/app/cadastro-editais/cadastro-editais.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-editais',
@@ -8,17 +14,18 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CadastroEditaisComponent {
 
-  checkbox = false;
+  filePdf: string = '';
+  checkbox: boolean = false;
   radio: string = '';
-  fileName: string = '';
+  fileName?: string;
   anexoForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.anexoForm = this.fb.group({
-      titulo: ['', Validators.required],
-      descreva: ['', Validators.required],
-      email: ['', Validators.required],
-      resultados: ['', Validators.required],
+      titulo: new FormControl('', Validators.required),
+      descreva: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.email),
+      resultados: [Boolean, Validators.required],
       filePdf: ['', Validators.required],
       anexos: this.fb.array([]),
     });
@@ -28,12 +35,18 @@ export class CadastroEditaisComponent {
     return this.anexoForm.get('anexos') as FormArray;
   }
 
+  anexoGroup(index: number) {
+    // console.log(this.anexos.get(String(index))?.value);
+
+    return this.anexos.get(String(index))?.get('file')!.value;
+  }
+
   newAnexo(): FormGroup {
     return this.fb.group({
       nameBt: ['', Validators.required],
       radio: ['', Validators.required],
-      link: ['', Validators.required],
-      file: ['', Validators.required],
+      link: '',
+      file: '',
     });
   }
 
@@ -49,15 +62,45 @@ export class CadastroEditaisComponent {
     return this.anexos.value[i].radio;
   }
 
-  upload(event: any) {
-    this.fileName = event.target.files[0].name;
-    console.log(this.anexos.value);
+  upload(event: any, index: number) {
+    if (event.target.files[0] != undefined) {
+      this.anexos
+        .get(String(index))
+        ?.patchValue({ file: event.target.files[0].name });
+    }else{
+      this.anexos.get(String(index))?.patchValue({file: ''});
+    }
+  }
+
+  uploadPdf(event: any) {
+    if(event.target.files[0] == undefined){
+      this.filePdf = '';
+    }else{
+      this.filePdf = event.target.files[0].name;
+    }
+  }
+
+  clearFile(index: number) {
+    this.anexos.get(String(index))?.patchValue({ file: '' });
+  }
+
+  clearPdf(){
+    this.filePdf = '';
+    this.anexoForm.get('filePdf')!.patchValue('');
+    console.log(this.filePdf);
   }
 
   radios(i: number) {
     this.radio = this.anexos.value[i].radio;
   }
 
+  invalidField(field: string) {
+    return (
+      this.anexoForm.controls[field].invalid &&
+      (this.anexoForm.controls[field].dirty ||
+        this.anexoForm.controls[field].touched)
+    );
+  }
 
   onSubmit() {
     console.log(this.anexoForm.value);
